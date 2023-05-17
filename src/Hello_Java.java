957ae2e6a8351e162697f7f49f7b5cc6b4b5a211
Your Name <you@example.com>
2023-05-17 23:37:39 +0300

    Added class Hello_Java

---
+++ src/Hello_Java.java
@@ -0,0 +1,12 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+
+
+public class Hello_Java {
+    public static void main(String[] args){
+       System.out.println("Hello, Java!");
+        System.out.print('1');
+        System.out.println("");
+        //System.out.print();    // Error
+    }
+}
