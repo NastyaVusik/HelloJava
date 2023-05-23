a9447d9ff5e2f508eb36595a5bcbeac7d36a6ab1
Anastasiya Vusik <nastyavusik13@gmail.com>
2023-05-23 18:32:51 +0300

    Added method getSymbol()

--- src/Lesson_3.java
+++ src/Lesson_3.java
@@ -1,14 +1,15 @@
 import java.util.Scanner;
+
 public class Lesson_3 {
     public static void main(String[] args) {
         getPersonInfo();
-        //getSymbol();
-        //getBodyMassIndex();
+        getSymbol();
+//        getBodyMassIndex();
     }
 
-/*Method for reading from the console and output person's info*/
+    /*Method for reading from the console and output person's info*/
 
-    public static void getPersonInfo(){
+    public static void getPersonInfo() {
         Scanner info = new Scanner(System.in);
         System.out.print("Input person name: ");
         String name = info.nextLine();
@@ -28,6 +29,22 @@ public class Lesson_3 {
         System.out.println("Age: " + age);
         System.out.println("Height: " + height);
         System.out.println("Weight: " + weight);
+    }
+
+
+    //Method for reading of symbol and convert it in a number
+
+    public static void getSymbol() {
+        Scanner symbol = new Scanner(System.in);
+
+        //Read symbol from Scanner;
+        System.out.print("Input one char: ");
+        char ch = symbol.next().charAt(0);
+
+        //Output of symbol ch in letter and digit format
+        System.out.println(ch + " - " + (int) ch);
+
+        symbol.close();
     }
 }
 
