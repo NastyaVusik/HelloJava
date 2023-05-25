16d86cb780a3140e3633cd0aeffcd52630261de2
Anastasiya Vusik <nastyavusik13@gmail.com>
2023-05-25 14:37:41 +0300

    Added method getDigits1To99()

--- src/Lesson_4_Loops.java
+++ src/Lesson_4_Loops.java
@@ -1,5 +1,19 @@
+//import java.util.Scanner;
+
 class Lesson_4_Loops {
     public static void main(String[] args) {
+        getDigits1To99();
 
     }
+
+    //Method for output only uneven numbers from 1 to 99
+    public static void getDigits1To99() {
+        for (int i = 1; i < 100; i++) {
+            if (i % 2 != 0) {
+                System.out.println(i);
+            }
+        }
+    }
+
+
 }
