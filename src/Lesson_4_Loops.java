745450273e41346d0dc35c3b9cf04a3881987688
Anastasiya Vusik <nastyavusik13@gmail.com>
2023-05-25 14:44:39 +0300

    Added method getDigits5To1()

--- src/Lesson_4_Loops.java
+++ src/Lesson_4_Loops.java
@@ -3,15 +3,25 @@
 class Lesson_4_Loops {
     public static void main(String[] args) {
         getDigits1To99();
+        getDigits5To1();
 
     }
 
-    //Method for output only uneven numbers from 1 to 99
+    //1. Method for output only uneven numbers from 1 to 99
     public static void getDigits1To99() {
-        for (int i = 1; i < 100; i++) {
+        for (int i = 1; i < 99; i++) {
             if (i % 2 != 0) {
                 System.out.println(i);
             }
+        }
+        System.out.println("****************");
+    }
+
+
+    //2. Method for output numbers from 5 to 1
+    public static void getDigits5To1() {
+        for (int i = 5; i > 1; i--) {
+            System.out.println(i);
         }
     }
 
