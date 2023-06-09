efc7222b7434857fac4fab115946f94192ae592e
Anastasiya Vusik <nastyavusik13@gmail.com>
2023-06-09 16:11:22 +0300

    Added method checkAddBanknotes() and AddBanknote()

--- src/ATM_machine/Atm.java
+++ src/ATM_machine/Atm.java
@@ -20,12 +20,43 @@ public class Atm {
         return new Scanner(System.in);
     }
 
+    //Method for checking denominate of banknotes
+    public boolean checkAddBanknotes(int addSum, int minBanknote, String... banknoteList) {
+        boolean res = false;
+
+        while (addSum != 0) {
+//            System.out.println(banknoteList);
+            res = true;
+            if (minBanknote >= 20) {
+                checkAddBanknotes(addSum - 20, 20, banknoteList + " [20] ");
+            }
+            if (minBanknote >= 50) {
+                checkAddBanknotes(addSum - 50, 50, banknoteList + " [50] ");
+            }
+            if (minBanknote >= 100) {
+                checkAddBanknotes(addSum - 100, 100, banknoteList + " [100] ");
+            }
+        }
+        System.out.println(banknoteList);
+        return res;
+    }
+
 
     //Method for filling ATM with banknotes
     public void AddBanknote(int addSum) {
-        System.out.println("Enter amount to fill AMT. Denomination of banknotes is 20, 50, 100. ");
+        System.out.println("Enter amount to fill AMT. Denomination of banknotes is 20, 50, 100 $. ");
         addSum = scan().nextInt();
-        addSum = banknote20 + banknote50 + banknote100;
+        while (addSum.checkAddBanknotes() != true) {
+            System.out.println("Your amount isn't correct. ATM give banknotes with denominate 20, 50, 100 $.");
+            addSum = scan().nextInt();
+        }
+        if (addSum.checkAddBanknotes() == true)
+            addSum += addSum;
+        System.out.println("Sum " + addSum + " $ was successfully added");
+    }
 
+    //Method for withdraw amount from the  ATM
+    public boolean withdrawAmount() {
+        System.out.println("Enter amount to withdraw from the AMT. Denomination of banknotes is 20, 50, 100 $. ");
     }
 }
