92e0c99c95db8aefd23f3d74892818a16e1a07dd
Anastasiya Vusik <nastyavusik13@gmail.com>
2023-06-13 01:28:56 +0300

    Completed class Circle.java

--- src/Shapes2D/Circle.java
+++ src/Shapes2D/Circle.java
@@ -1,4 +1,23 @@
 package Shapes2D;
 
-public class Circle {
+public class Circle extends Rectangle {
+    private double square;
+    private double perimeter;
+
+    public Circle(double height, double width) {
+        super(height, width);                //width is radius of circle
+        this.square = square;
+        this.perimeter = perimeter;
+    }
+
+
+    public String countSquare() {
+        square = Math.PI * width * width;
+        return "Square of the circle is " + String.format("%.2f", square) + " sm2";
+    }
+
+    public String countPerimeter() {
+        perimeter = 2 * Math.PI * width;
+        return "Length of circle is " + String.format("%.2f", perimeter) + " sm";
+    }
 }
--- src/Shapes2D/MainShapes2D.java
+++ src/Shapes2D/MainShapes2D.java
@@ -6,6 +6,7 @@ public class MainShapes2D {
         //Created objects
         Shape triangle1 = new Triangle(6.5, 8.1);
         Triangle rectangle1 = new Rectangle(9.12, 12.3);
+        Rectangle circle1 = new Circle(1, 5);     //????????????????????Why variable heigth = 1` doesn't influence on calculation
 
         //Output info about the shapes
         System.out.println("\n*******************************************************\n");
@@ -19,6 +20,12 @@ public class MainShapes2D {
         rectangle1.printVar();
         System.out.println(rectangle1.countSquare());
         System.out.println(rectangle1.countPerimeter());
+        System.out.println("\n*******************************************************\n");
+
+        System.out.println("Circle circle1:");
+        rectangle1.printVar();
+        System.out.println(circle1.countSquare());
+        System.out.println(circle1.countPerimeter());
         System.out.println("\n*******************************************************\n");
 
 
