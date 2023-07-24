8362f694ae0173c90953e8f4fd0b04d19974bdd6
Anastasiya Vusik <nastyavusik13@gmail.com>
2023-07-25 01:28:11 +0300

    Method  readDocNumbers() was completed

--- src/FilesWithDocuments/newFiles/FilesReader.java
+++ src/FilesWithDocuments/newFiles/FilesReader.java
@@ -2,10 +2,9 @@ package FilesWithDocuments.newFiles;// Press Shift twice to open the Search Ever
 // then press Enter. You can now see whitespace characters in your code.
 
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Scanner;
 
@@ -27,7 +26,7 @@ public class FilesReader {
 //    в строке никакой другой информации, только номер документа.
 //    Валидный номер документа должен иметь длину 15 символов и
 //    начинаться с последовательности docnum(далее любая
-//последовательность букв/цифр) или kontract(далее любая
+//последовательность букв/цифр) или contract(далее любая
 //    последовательность букв/цифр).
 //    Учесть, что номера документов могут повторяться в пределах одного
 //    файла и так же разные документы могут содержать одни и те же номера
@@ -59,8 +58,14 @@ public class FilesReader {
 
         createFiles();
         System.out.println("\n\n*************************************************\n\n");
+        readDocNumbers();
+        System.out.println("\n\n*************************************************\n\n");
+
 
     }
+
+    //Create variable filePath
+    static String filePath;
 
     //Method for Scanner calling
     static String enterFileInfo() {
@@ -68,21 +73,18 @@ public class FilesReader {
     }
 
     //Method for creating new files and putting them into ArrayList
-    static void createFiles() throws IOException {
-
-        //Create scanner for reading path to the file and file name
-        Scanner readFilePath = new Scanner(System.in);
+    static ArrayList<String> createFiles() throws IOException {
 
         //Create ArrayList for keeping files' path and names
         ArrayList<String> filesList = new ArrayList<>();
 
         //Create variable filePath
-        String filePath;
+//        String filePath;
 
         //Read file path and file name from the console before digit zero is entered
         while (true) {
             System.out.print("Enter path to the file and file's name: ");
-            filePath = readFilePath.nextLine();
+            filePath = enterFileInfo();
 
             if (filePath.equals("0")) {
                 break;
@@ -98,8 +100,8 @@ public class FilesReader {
 
                 FileWriter writer = new FileWriter(newFile, true);              //Create object writer to write document's numbers into the file
                 System.out.print("Write a text to save in the document: ");
-//                writer.write(String.valueOf(readFilePath));             //???????????????Why is valueOf required here?
-                writer.write(enterFileInfo());             //???????????????Why is valueOf required here?
+
+                writer.write(enterFileInfo());
 
                 writer.flush();
                 writer.close();
@@ -110,14 +112,52 @@ public class FilesReader {
                 throw new RuntimeException(e);
             }
         }
-        readFilePath.close();
+
+        return filesList;
+
     }
 
-    //Method for reading document's numbers from the files and validating this numbers
-    static String readDocNumbers() {
+
+    //Getters and Setters
+    public static String getFilePath() {
+        return filePath;
+    }
+
+    public static void setFilePath(String filePath) {
+        FilesReader.filePath = filePath;
+    }
+
+
+    //Method for reading document's numbers from the files and saving them in collection Set
+    static HashSet<String> readDocNumbers() throws IOException {
 
         //Create collection HashSet for keeping files' path and names
         HashSet<String> docList = new HashSet<String>();
+
+        try (FileReader reader = new FileReader()) {
+            for (int i = 0; i < filesList.size(); i++) {
+                String fileDocNumbers = reader.read(filesList.get(i));
+                docList.add(fileDocNumbers);
+            }
+        } catch (IOException e){
+            e.getStackTrace();
+        }
+return docList;
+
+    }
+
+
+    //Method for reading document's numbers from the files and validating this numbers
+    static HashMap writeResults() {
+
+        //Create collection HashMap for keeping files' path and names
+        HashMap<> docList = new HashMap();
+
+        System.out.print("Enter path to the file Report and file's name: ");
+        filePath = enterFileInfo();
+
+        //Create file for writing all unique document's numbers
+        File fileReport = new File(filePath);
 
     }
 
