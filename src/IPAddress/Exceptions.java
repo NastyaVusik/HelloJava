929353f3185a28144c69e428a54d17f376f3aeac
Anastasiya Vusik <nastyavusik13@gmail.com>
2023-08-07 23:11:30 +0300

    Four classes were created

---
+++ src/IPAddress/Exceptions.java
@@ -0,0 +1,4 @@
+package IPAddress;
+
+public class Exceptions {
+}
--- src/IPAddress/MainIPAddress.java
+++ src/IPAddress/MainIPAddress.java
@@ -1,10 +1,10 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+package IPAddress;// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 
 
-public class Hello_Java {
-    public static void main(String[] args){
-       System.out.println("Hello, Java!");
+public class MainIPAddress {
+    public static void main(String[] args) {
+        System.out.println("Hello, Java!");
         System.out.print('1');
         System.out.println(" ");
         //System.out.print();    // Error
