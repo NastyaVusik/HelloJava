3dada75b445fffd6637551aed4500ffd21d39d71
Anastasiya Vusik <nastyavusik13@gmail.com>
2023-08-08 11:10:50 +0300

    Class MainFolderWithDocuments.java was created

--- src/IPAddress/ConsoleReader.java
+++ src/IPAddress/ConsoleReader.java
@@ -2,14 +2,14 @@ package IPAddress;
 
 import java.util.Scanner;
 
-public class ConsoleReader {
-
-    //Create object of class Scanner
-    Scanner scanner = new Scanner(System.in);
-
-    public String getConsoleReader() {
-        return scanner.nextLine();
-    }
-
-
-}
+//public class ConsoleReader {
+//
+//    //Create object of class Scanner
+//    Scanner scanner = new Scanner(System.in);
+//
+//    public String getConsoleReader() {
+//        return scanner.nextLine();
+//    }
+//
+//
+//}
