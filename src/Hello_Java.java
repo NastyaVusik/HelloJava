5bb281b5a9bc5608b48c32a2d6911fd1c0daaa9f
Anastasiya Vusik <nastyavusik13@gmail.com>
2024-01-17 18:18:02 +0300

    Some bugs were fixed

--- src/Hello_Java.java
+++ src/Hello_Java.java
@@ -2,13 +2,39 @@
 // then press Enter. You can now see whitespace characters in your code.
 
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Hello_Java {
-    public static void main(String[] args){
-       System.out.println("Hello, Java!");
-        System.out.print('1');
-        System.out.println(" ");
-        //System.out.print();    // Error
-        System.out.print("Hello, Java!");
+    public static void main(String[] args) {
+        Set<String> qqq = addToLovelyCurrencies("m", "x");
+
+        System.out.println(qqq);
+    }
+
+    public static Set<String> addToLovelyCurrencies(String fromCurrency, String toCurrency) {
+        Set<String> lovelyCurrencies = new HashSet<>();
+        int limit = 6;
+
+        lovelyCurrencies.add("a");
+        lovelyCurrencies.add("b");
+        lovelyCurrencies.add("c");
+        lovelyCurrencies.add("d");
+        lovelyCurrencies.add("e");
+        lovelyCurrencies.add("g");
+        lovelyCurrencies.add("h");
+        lovelyCurrencies.add("k");
+
+
+        while ((!lovelyCurrencies.isEmpty()) & (lovelyCurrencies.size() < limit)) {
+            lovelyCurrencies.remove(lovelyCurrencies.stream().iterator().next());
+            lovelyCurrencies.remove(lovelyCurrencies.stream().iterator().next());
+            lovelyCurrencies.add(fromCurrency);
+            lovelyCurrencies.add(toCurrency);
+        }
+
+        return lovelyCurrencies;
     }
 }
+
 
